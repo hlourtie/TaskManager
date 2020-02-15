e60337bd161e48857db5234bf7a20b9fb5e0bfbc
Henry Lourtie <henry.lourtie@gmail.com>
2020-02-15 15:43:03 +0100

    this is the begining of tk

--- src/main.py
+++ src/main.py
@@ -1,3 +1,17 @@
 import tkinter as tk
+from tkinter import *
+# window = tk.Tk()
+# window.title("TimeMaster")
+# window.mainloop()
+f = open("Time.txt","w+")
+window = tk.Tk()
+window.title("TimeMaster")
+def add_time(window):
+	txt = Entry(window, width=10)
+	txt.pack(side = BOTTOM)
 
-tk._test()
+bt = tk.Button(window,text = "Add", command = add_time(window))
+bt.pack(side = BOTTOM)
+window.mainloop()
+
+	
