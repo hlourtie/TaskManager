f464dd51f588a805b8c1f038a9a7d6e7bd225d9a
Henry Lourtie <henry.lourtie@gmail.com>
2020-02-16 19:11:10 +0100

    adding objects should work fine

--- src/main.py
+++ src/main.py
@@ -1,16 +1,27 @@
 import tkinter as tk
 from tkinter import *
+from datetime import datetime
+from time import *
 # window = tk.Tk()
 # window.title("TimeMaster")
 # window.mainloop()
+int i = 0
+
+class timing():
+	def __init__(self,name,time):
+		self.name = name
+		self.time = time
+		self.date = datetime.date(datetime.now())
+	
+	
+def add_time():
+	txt = Entry(window, width=10)
+	txt.grid(row = i, column = 0)
+	label
 f = open("Time.txt","w+")
 window = tk.Tk()
 window.title("TimeMaster")
-def add_time(window):
-	txt = Entry(window, width=10)
-	txt.pack(side = BOTTOM)
-
-bt = tk.Button(window,text = "Add", command = add_time(window))
+bt = tk.Button(window,text = "Add", command = add_time)
 bt.pack(side = BOTTOM)
 window.mainloop()
 
