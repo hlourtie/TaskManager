ec100946f380d74753145a97a910617e9a8b8b6e
Henry Lourtie <henry.lourtie@gmail.com>
2020-02-18 22:30:57 +0100

    added the buttons and timing object

--- src/main.py
+++ src/main.py
@@ -2,27 +2,50 @@ import tkinter as tk
 from tkinter import *
 from datetime import datetime
 from time import *
+import time
+import datetime
 # window = tk.Tk()
 # window.title("TimeMaster")
 # window.mainloop()
-int i = 0
-
-class timing():
-	def __init__(self,name,time):
+i = 0
+records = []
+class Timing(object):
+	start = 0
+	elapsed = 0
+	date = ""
+	def __init__(self,name):
 		self.name = name
-		self.time = time
-		self.date = datetime.date(datetime.now())
-	
+		self.date = datetime.datetime.now()
+	def set_name(name):
+		self.name = name
+	def set_start(_):
+		start = time.time()
+	def set_elapsed(_):
+		if elapsed != 0:
+			elapsed += time.time() - self.start
+		elapsed = time.time() - self.start
+	def save_me(_):
+		f.write("Customer: %s , date %d, total time %d", name, date, elapsed)
+
 	
 def add_time():
 	txt = Entry(window, width=10)
 	txt.grid(row = i, column = 0)
-	label
+	records.append(Timing(txt.get))
+	beginButton = tk.Button(window, text="Start", command= records[i].set_start)
+	beginButton.grid(row = i, column = 1)
+	stopButton = tk.Button(window, text="Stop", command= records[i].set_elapsed)
+	stopButton.grid(row = i, column = 2)
+	saveButton = tk.Button(window, text="Save", command= records[i].save_me)
+	saveButton.grid(row = i, column = 3)
+	
+
 f = open("Time.txt","w+")
 window = tk.Tk()
 window.title("TimeMaster")
 bt = tk.Button(window,text = "Add", command = add_time)
-bt.pack(side = BOTTOM)
+bt.grid(sticky="s")
+f.close()
 window.mainloop()
 
 	
